c381bc75a4947809b198e7c679b66d34f38b2e65
Wang Luo <lw1107034863@126.com>
2024-07-18 12:14:53 +0800

    update

--- sklearn_mode.py
+++ sklearn_mode.py
@@ -11,6 +11,10 @@ import time
 import numpy as np  
 import scipy.io as sio  
 import sklearn
+from sklearn.model_selection import GridSearchCV  
+from sklearn.svm import SVC
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
 
 # 测试样本对应的真实格点坐标
 def test_real(path):
@@ -69,8 +73,7 @@ def logistic_regression_classifier(train_x, train_y):
   
   
 # Random Forest Classifier  
-def random_forest_classifier(train_x, train_y):  
-    from sklearn.ensemble import RandomForestClassifier  
+def random_forest_classifier(train_x, train_y):    
     model = RandomForestClassifier(n_estimators=8)  
     model.fit(train_x, train_y)  
     return model  
@@ -93,23 +96,20 @@ def gradient_boosting_classifier(train_x, train_y):
   
   
 # SVM Classifier  
-def svm_classifier(train_x, train_y):  
-    from sklearn.svm import SVC  
+def svm_classifier(train_x, train_y):    
     model = SVC(kernel='rbf', probability=True)  
     model.fit(train_x, train_y)  
     return model  
   
 # SVM Classifier using cross validation  
-def svm_cross_validation(train_x, train_y):  
-    from sklearn.grid_search import GridSearchCV  
-    from sklearn.svm import SVC  
+def svm_cross_validation(train_x, train_y):    
     model = SVC(kernel='rbf', probability=True)  
     param_grid = {'C': [1e-3, 1e-2, 1e-1, 1, 10, 100, 1000], 'gamma': [0.001, 0.0001]}  
     grid_search = GridSearchCV(model, param_grid, n_jobs = 1, verbose=1)  
     grid_search.fit(train_x, train_y)  
     best_parameters = grid_search.best_estimator_.get_params()  
     for para, val in best_parameters.items():  
-        print para, val  
+        print(para, val) 
     model = SVC(kernel='rbf', C=best_parameters['C'], gamma=best_parameters['gamma'], probability=True)  
     model.fit(train_x, train_y)  
     return model  
@@ -164,21 +164,21 @@ if __name__ == '__main__':
                  'GBDT':gradient_boosting_classifier,
     }  
       
-    print 'reading training and testing data...'  
+    print('reading training and testing data...' ) 
     train_x, train_y, test_x, test_y = read_data(path)  
     num_train, num_feat = train_x.shape  
     num_test, num_feat = test_x.shape  
     is_binary_class = (len(np.unique(train_y)) == 56)  
-    print '******************** Data Info *********************'  
-    print '#training data: %d, #testing_data: %d, dimension: %d' % (num_train, num_test, num_feat)  
+    print('******************** Data Info *********************') 
+    print('#training data: %d, #testing_data: %d, dimension: %d'%(num_train, num_test, num_feat))   
     
     predict_total2=[]
     rmse_total=[]
     for classifier in test_classifiers:  
-        print '******************* %s ********************' % classifier  
+        print('******************* %s ********************' % classifier) 
         start_time = time.time()  
         model = classifiers[classifier](train_x, train_y)  
-        print 'training took %fs!' % (time.time() - start_time)  
+        print('training took %fs!' % (time.time() - start_time))
         predict = model.predict(test_x)  
         if model_save_file != None:  
             model_save[classifier] = model  
@@ -187,10 +187,10 @@ if __name__ == '__main__':
 #            recall = metrics.recall_score(test_y, predict)  
 #            print 'precision: %.2f%%, recall: %.2f%%' % (100 * precision, 100 * recall)  
             tmp = func_rmse(predict,test_y)
-            print tmp
+            print(tmp)
             rmse_total.append(tmp)
         accuracy = sklearn.metrics.accuracy_score(test_y, predict)  
-        print 'accuracy: %.2f%%' % (100 * accuracy)   
+        print('accuracy: %.2f%%' % (100 * accuracy))  
         predict_total2.append(predict)
     predict_total1 = np.array(predict_total2)
     predict_total = predict_total1.transpose()
